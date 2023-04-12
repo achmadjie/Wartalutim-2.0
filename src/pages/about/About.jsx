09c5b076417523799cfd86213d9b74eff4868a65
Andi Adjie <achmdadjie@gmail.com>
2023-04-12 13:30:11 +0700

    create about us page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import logo_about from "../../assets/images/logo_about.png"
+import styles from "./about.module.css";
 
 export default function About() {
   return (
-	<div>About</div>
-  )
+    <div className={styles.about_container}>
+      <img src={logo_about} alt="about-logo" />
+      <div className={styles.about_desc_container}>
+        <h3 className={styles.about_text}>Tentang kami</h3>
+        <p className={styles.about_desc_text}>
+          Warta Lutim hadir sebagai media online yang bertujuan untuk memberikan
+          informasi yang akurat, terpercaya, dan bermanfaat bagi masyarakat
+          kabupaten Luwu Timur dan wilayah sekitarnya. Dalam menjalankan
+          misinya, Warta Lutim senantiasa mengutamakan prinsip keberagaman dan
+          etika jurnalistik yang tinggi, sehingga dapat memberikan manfaat yang
+          sebesar-besarnya bagi pembaca.
+        </p>
+      </div>
+    </div>
+  );
 }
